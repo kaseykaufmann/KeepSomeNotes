cf7b7e8bf7816a93ba9e6390f68683e9d9369acb
Kasey <kaseykaufmann@yahoo.com>
2021-02-05 15:28:58 -0500

    Switched to react

--- public/app/content.js
+++ public/app/content.js
@@ -3,7 +3,7 @@ console.log("This is the test");
 let paragraphs = document.getElementsByTagName("p");
 
 for (elt of paragraphs) {
-  elt.style["background-color"] = "#FF00FF";
+  elt.style["background-color"] = "white";
 }
 
 const gotMessage = (request, sender, response) => {
